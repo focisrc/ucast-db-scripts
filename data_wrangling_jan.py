bd1717fa3d74ed9484589190aea63ab042bcb179
phanicode <63126072+phanicode@users.noreply.github.com>
2022-01-26 03:49:56 -0700

    For Jann 26 meeting

---
+++ data_wrangling_jan.py
@@ -0,0 +1,117 @@
+import os
+import numpy as np ,pandas as pd , matplotlib.pyplot as plt
+import scipy as sci
+from scipy import signal
+import os
+from itertools import groupby
+from statsmodels.stats.weightstats import DescrStatsW
+TIME_START="20:00"
+TIME_END="03:00"
+
+filename="SMT_data_edit.csv"
+
+#Need
+def load(month):
+  # month: integer representing month of year
+    # Filter by hours 8 pm to 3 am
+    df = pd.read_csv(filename, error_bad_lines=False,header=0,usecols = ['dateStart','tau'])
+    df.index=pd.to_datetime(df.index)
+    ndf=df.between_time(TIME_START,TIME_END)
+    # January
+    monthdf =ndf[ndf.index.month == month]
+    # First week
+
+    week1df = monthdf[(monthdf.index.isocalendar().week==1) | (monthdf.index.isocalendar().week==2)| (monthdf.index.isocalendar().week==3)]
+    week1df["site"]="SMT"
+    return week1df
+week1df=load(1)
+
+def day_stats(tau,df):
+    # Custom function to solve this sample problem
+    # Threshold : 5
+    # input: [0,1,3,4,5,5.1,5.1,0,3,9] 
+    # output: [5,2]
+    # Process: Gets all the lengths of consecutive numbers less than threshold
+    # We need this for the Tau vs time p
+    # Returns the mean, standard deviation and length of observing period to account for discrepancies in number of observations
+    c = df['tau'].le(tau)
+    lengths=[]
+    for k, g in groupby(c.values):
+            g = list(g)
+            if k==1:
+                lengths.append(len(g))
+    return lengths
+
+
+def get_split_locs(df):
+  # This gets the index values for dataframe to be split by observation night
+
+    compare_dt= list(df.index)
+    del compare_dt[0]
+    i=0
+    split_list=[]
+    for past,present in zip(compare_dt,df.index):
+        # A timedelta of > 1 hr means that data is discontinuous ( intended as in between_time(10:00-3:00) ) and it is a new day
+        time_delta=past-present 
+        if time_delta > pd.Timedelta("1 hour") and time_delta < pd.Timedelta("360 days"):
+           
+            # print(past,present,past-present)
+            split_list.append(i)
+        i+=1
+    return split_list
+
+def split_df(locs,df):
+    # This splits the dataframe with the loc values from get_split_locs
+    ldf=[]
+    ldf.append(df.iloc[:locs[0],:])
+
+    for i in range(len(locs)-1):
+        ldf.append(df.iloc[locs[i]:locs[i+1],:])
+    ldf.append(df.iloc[locs[0]:,:])
+    return ldf
+
+
+def plot(taus,means,sds):
+    plt.xlabel("Tau")
+    plt.ylabel("Time(T> Tau) in hours")
+    plt.title("SMT First week of January ,{}:{}, Tau vs time, median_filter={}".format(TIME_START,TIME_END,FILTER_TIME))
+    plus=[x + y for x, y in zip(means, sds)]
+    minus=[x - y for x, y in zip(means, sds)]
+    plt.plot(taus,plus,label="+std")
+    plt.plot(taus,minus,label="-std")
+    plt.plot(taus,means,label="avg" )
+    plt.yticks(np.arange(0, 9, step=1))
+    plt.xlim(0.05,0.6)
+    plt.axvline(x=0.3,color="red")
+    plt.legend(loc="upper left")
+    plt.grid(True)
+    plt.show()
+# Loading the first week of January and the whole month
+# We only use week1df in the rest of the script , feel free to play around with monthdf ( in this case january df)
+
+# We split the dataframe into a list of dataframes with each observing period 
+loc_list=get_split_locs(week1df)
+df_list=split_df(loc_list,week1df)
+
+# Deleting the final df (as it is the whole df and fixes an off by 1 error )
+del df_list[-1]
+# Tau values (Accuracy can be improved by changed 0.1 to 0.01)
+taus=np.arange(0,0.5,0.01)
+# Making a dictionary of aggregated statistics for each day
+stats=dict()
+for tau in taus:
+    stats[tau]=[]
+for day in df_list:
+    for tau in taus:
+        stats[tau].extend((day_stats(tau,day)))
+
+
+
+means=[]
+sds=[]
+for tau in taus[1:]:
+    means.append(np.mean(stats[tau])/12)
+    sds.append(np.std(np.absolute(stats[tau]))/12)
+    
+
+plot(taus[1:],means,sds)
